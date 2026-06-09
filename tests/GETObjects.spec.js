f50aa53a6ac1ba5ac1b23f57759417df53b9c742
Abhishek Tyagi <ais.abhi19@gmail.com>
2026-06-09 17:44:27 +0530

    Pratice response code/Text checks for API

--- tests/GETObjects.spec.js
+++ tests/GETObjects.spec.js
@@ -6,8 +6,18 @@ import { test, expect } from "@playwright/test";
 test("Get API ", async ({ request }) => {
   const response = await request.get("https://api.restful-api.dev/objects/1");
   const body = await response.json();
-  const Responsestatus = await response.status();
-  //console.log(body);
-  console.log(Responsestatus);
-  expect(Responsestatus).toBe(200);
+  const ResponseStatus = response.status();
+  const ResponseText = await response.statusText();
+
+  console.log(body);
+  console.log(ResponseStatus);
+
+  console.log(body.id);
+  console.log(body.name);
+  console.log(body.data);
+  console.log(body.data.color);
+  expect(body.id).toBe("1");
+  expect(body.name).toBe("Google Pixel 6 Pro");
+  expect(ResponseStatus).toBe(200); // Check API Reposnse
+  expect(ResponseText).toBe("OK"); // Check API Reposnse Text
 });
