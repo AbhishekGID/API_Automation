51d3bcd3ed52e12bdb4bbb343ffff03b880c2a84
Abhishek Tyagi <ais.abhi19@gmail.com>
2026-06-11 18:59:41 +0530

    Practice POST Request

--- tests/GETObjects.spec.js
+++ tests/GETObjects.spec.js
@@ -3,7 +3,7 @@
 
 import { test, expect } from "@playwright/test";
 
-test("Get API ", async ({ request }) => {
+test.skip("Get API ", async ({ request }) => {
   const response = await request.get("https://api.restful-api.dev/objects/1");
   const body = await response.json();
   const ResponseStatus = response.status();
@@ -19,5 +19,30 @@ test("Get API ", async ({ request }) => {
   expect(body.name).toBe("Google Pixel 6 Pro");
   expect(ResponseStatus).toBe(200); // Check API Reposnse
   expect(ResponseText).toBe("OK"); // Check API Reposnse Text
-  expect(body).not.toBeNull(); // Check Repsone Body is NOT NULL
+  expect(body).not.toBeNull(); // Check Repsone Body is NOT
+});
+
+test("POST API ", async ({ request }) => {
+  const payload = {
+    name: "Apple MacBook Pro 16",
+    data: {
+      year: 2019,
+      price: 1849.99,
+      "CPU model": "Intel Core i9",
+      "Hard disk size": "1 TB",
+    },
+  };
+
+  const endpoint = "https://api.restful-api.dev/objects";
+  const response = await request.post(endpoint, {
+    headers: { "Content-Type": "application/json" },
+    data: payload,
+  });
+
+  const Reqbody = await response.json();
+  console.log(Reqbody);
+
+  const GETResponse = request.get(
+    "https://api.restful-api.dev/objects/ff8081819d82fab6019eb6da5b887255"
+  );
 });
