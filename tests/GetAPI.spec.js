8510ce6de2507b49928128f76f907a35aa8b1b9d
AbhishekGID <ais.abhi19@gmail.com>
2026-07-02 18:04:24 +0530

    API Automation Practice

--- tests/GetAPI.spec.js
+++ tests/GetAPI.spec.js
@@ -20,5 +20,10 @@ test("Get Booking Deatils with ID GET Request", async ({ request }) => {
   const GetresponseBody = await request.get("https://restful-booker.herokuapp.com/booking/1");
   const ReponseData = await GetresponseBody.json()
   console.log(ReponseData)
+  console.log("--------------")
+  console.log(GetresponseBody.statusText())
+  // Check StatusText 
+  expect(GetresponseBody.statusText()).toBe('OK')
+
 
 });
